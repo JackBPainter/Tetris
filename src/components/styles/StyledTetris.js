9ac678b75ca3b5a9ddcfe5a0d3217adaca266266
Jack Painter <littlesympathy@jacks-mbp.home>
2019-10-04 18:10:39 +0100

    Create StyledTetris Component

---
+++ src/components/styles/StyledTetris.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+import bgImg from '../../image/bg.png';
+import Display from '../Display';
+
+export const StyledTetrisWrapper = styled.div`
+    height: 100vh;   
+    width: 100vw;
+    background: url(${bgImg});
+    background-size: cover;
+    overflow: hidden;
+`
+
+export const StyledTetris = styled.div`
+    display: flex;
+    align-items: flex-start;
+    margin: 0 auto;
+    padding: 50px;
+    max-width: 900px;
+
+
+    aside {
+        width: 100%;
+        max-width: 200px;
+        display: block;
+        padding: 0 20px;
+    }
+`
