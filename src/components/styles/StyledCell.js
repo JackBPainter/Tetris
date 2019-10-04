6440adbba09199e3aad0d9b7d1a6248875698993
Jack Painter <littlesympathy@jacks-mbp.home>
2019-10-04 17:40:42 +0100

    Add border detail to StyledCell component

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 export const StyledCell = styled.div`
     width: auto;
     background: rgba(${props => props.color}, 0.8);
+    border: ${props => (props.type === 0 ? '0px': '2px solid')};
+    border-bottom-color: rgba(${props => props.color}, 0.2);
+    border-left-color: rgba(${props => props.color}, 1.2);
+    border-right-color: rgba(${props => props.color}, 0.2);
+    border-top-color: rgba(${props => props.color}, 1.2);
 `
