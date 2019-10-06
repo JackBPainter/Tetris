ea27d975da3aba590d137667d2fd6da7ba9ee1e7
Jack Painter <littlesympathy@jacks-mbp.home>
2019-10-06 12:40:17 +0100

    Create StyledStage component

---
+++ src/components/styles/StyledStage.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledStage = styled.div`
+    display: grid;
+    grid-template-rows: repeat(
+        ${props => props.height},
+        calc(25vw / ${props => props.width})
+    );
+    grid-template-columns: repeat(${props => props.width}, 1fr);
+    grid-gap: 1px;
+    border: 2px solid #333;
+    width: 100%;
+    max-width: 25vw;
+    background: #111;
+`;
+
+/* 
+    The template rows / columns is used to keep
+    the cells square and manage the aspect ratio of 
+    the grid cells
+*/
