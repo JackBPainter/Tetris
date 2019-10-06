4651a82e3a3bbbc9b5d8dd9d52522c206d667ad0
Jack Painter <littlesympathy@jacks-mbp.home>
2019-10-06 13:46:04 +0100

    Import hooks to Tetris & create Game Over ternary

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+/* Styled Components*/
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
+/* Hooks */
+import { useStage } from '../hooks/useStage';
+import { usePlayer } from '../hooks/usePlayer';
+
+/* Components */
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+    const [speed, setSpeed] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-            <Stage stage={createStage()} />
+            <Stage stage={stage} />
                 <aside>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
                     <div>
                         <Display text="Score" />
                         <Display text="Rows" />
                         <Display text="Level" />
                     </div>
+                    )}
                 <StartButton />
                 </aside>
             </StyledTetris>
